c4410a14638bb0f25b89c4905b205e6496d6b092
yves <yvesmomplais@hotmail.com>
2021-02-22 22:13:30 -0500

    commit tree c6b3625a9e88355ec5fa7ed29616b25e444335a4

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,79 @@
 class Owner
-  # code goes here
-end
+    attr_accessor :cats, :dogs
+    attr_reader :name, :species
+  
+
+    @@all = []
+  
+
+    def initialize(name)
+      @name = name 
+      @species = "human"  
+      @@all << self
+    end    
+  
+
+    def say_species
+      "I am a #{species}."
+    end 
+  
+
+    def self.all
+      @@all
+    end 
+  
+
+    def self.count
+      @@all.length
+    end 
+  
+
+    def self.reset_all
+      self.all.clear
+    end 
+  
+
+    def cats
+      Cat.all.select {|cat| cat.owner == self}
+    end 
+  
+
+    def dogs
+      Dog.all.select {|dog| dog.owner == self}
+    end 
+  
+
+    def buy_cat(cat_name)
+      Cat.new(cat_name, self)
+    end 
+  
+
+    def buy_dog(dog_name)
+      Dog.new(dog_name, self)
+    end 
+  
+
+    def walk_dogs
+      self.dogs.each {|dog| dog.mood ="happy"}
+    end 
+  
+
+    def feed_cats
+      self.cats.each {|cat| cat.mood ="happy"}
+    end 
+  
+
+    def sell_pets
+      self.dogs.each {|dog| dog.mood ="nervous"} 
+      self.cats.each {|cat| cat.mood ="nervous"} 
+      self.dogs.map {|dog| dog.owner = nil }
+      self.cats.map {|cat| cat.owner = nil }  
+    end 
+  
+
+    def list_pets
+      "I have #{self.dogs.count} dog(s), and #{self.dogs.count} cat(s)."
+    end 
+  
+
+  end
